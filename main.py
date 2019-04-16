ab96d4443f014c328286cba3a57ef4d9aae7ba6b
Shivam Rawat <shivamrawat6670@gmail.com>
2019-04-16 22:04:58 +0530

    Change

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 import requests
 import csv
 from bs4 import BeautifulSoup
-
-
+page=[]
+soup=[]
+links =["https://seizoroi.com/product_api/0999/0999_dk.htm","https://seizoroi.com/product_api/0999_white.htm"]
 class CheckInInventory:
     def __init__(self):
-        page=requests.get('https://seizoroi.com/product_api/0999/0999_dk.htm?fbclid=IwAR1rBj-I_qp72HtiTmk0zdd9XCN6Qz_tfDrEKfVIFdAZR0HAq3M4qGTFQcs')
-        soup= BeautifulSoup(page.content,'html.parser')
-        self.all_columns=soup.find_all(class_="cBeditable")
+        page.append(requests.get(links[0]))
+        page.append(requests.get(links[1]))
+        soup.append(BeautifulSoup(page[0].content,'html.parser'))
+        soup.append(BeautifulSoup(page[1].content,'html.parser'))
+        self.all_columns=soup[0].find_all(class_="cBeditable")
+        self.all_columns_sec=soup[1].find_all(class_="cBeditable")
+
         self.size_dictionary={"2XS":0,"XS":1,"S":2,"M":3,"L":4,"XL":5,"2XL":6,"3XL":7}
+        self.size_dictionary_sec={"28":0,"29":1,"30":2,"31":3,"32":4,"33":5,"34":6,"35":7,"36":8,"37":9,"38":10,"39":11,"40":12,"41":13,"42":14,"43":15,"44":16,"45":17,"46":18,"47":19,"48":20,"49":21}
     def checkProduct(self):
         with open('./prod.csv','rb') as input_file:
             reader = csv.reader(input_file)
             for row in reader:
+                if(row[1]==links[0]):
+                    self.main_column=self.all_columns
+                    self.main_dictionary = self.size_dictionary
+                elif(row[1]==links[1]):
+                    self.main_column=self.all_columns_sec
+                    self.main_dictionary = self.size_dictionary_sec
                 id_prod=row[0].split('/')[1]
-                for single_column in self.all_columns:
+                for single_column in self.main_column:
                     if id_prod==single_column.find(class_="color-name").get_text().split(' ')[0]:
-                        num=int(self.size_dictionary[row[0].split('/')[2]])
+                        num=int(self.main_dictionary[row[0].split('/')[2]])
                         available=int((single_column.find(class_="cBsecondLine").find_all(class_="assortTypeFixed"))[num].get_text().replace("+","").strip())
                         if(available>0):
                             print(row[0]+" product is available "+str(available))
-                        if(available==0):
+                        elif(available==0):
                             print(row[0]+" product is not available")
                             
 
