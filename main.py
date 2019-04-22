9629859adb940d396fe84adcf278adc104c08af7
Bence Ladoczki <info@ldczk.com>
2019-04-22 17:20:46 +0900

    add

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import requests
 import csv
 import random
@@ -9,46 +10,40 @@ class CheckInInventory:
         self.columns=[]
         self.size_dictionary=[]
         self.read_links();
-        for link in self.links:
-            self.pages.append(requests.get(link))
-        for page in self.pages:
-            self.soups.append(BeautifulSoup(page.content,'html.parser'))
-        for soup in self.soups : 
-            self.columns.append(soup.find_all(class_="cBeditable"))
+
         self.size_dictionary.append({"2XS":0,"XS":1,"S":2,"M":3,"L":4,"XL":5,"2XL":6,"3XL":7})
         self.size_dictionary.append({"28":0,"29":1,"30":2,"31":3,"32":4,"33":5,"34":6,"35":7,"36":8,"37":9,"38":10,"39":11,"40":12,"41":13,"42":14,"43":15,"44":16,"45":17,"46":18,"47":19,"48":20,"49":21})
+    def get_inventory_info(self,sku,link):
+     print("Now getting inventory info for: "+str(sku))
+     content = requests.get(link).content
+     soup = BeautifulSoup(content,'html.parser')
+     column = soup.find_all(class_="cBeditable")
+
+     id_prod = sku
+# TODO
+# Write your logic here to get the inventory info for an SKU from the HTML content
+
+     for single_column in column:
+      if id_prod==single_column.find(class_="color-name").get_text().replace("\t"," ").split(' ')[0]:
+       num = int(self.main_dictionary[row[0].split('/')[2]])
+       inventory_info = int((single_column.find(class_="cBsecondLine").find_all(class_="assortTypeFixed"))[num].get_text().replace("+","").strip())
+       return inventory_info
+
     def read_links(self):
         self.links=[]
         with open('./prod.csv','rb') as input_file:
             reader = csv.reader(input_file)
             for row in reader:
-                if(row[1] not in self.links):
-                    self.links.append(row[1])
+              print(str(self.get_inventory_info(row[0],row[1])))
+        sys.exit(1)
+            
             #self.links=random.sample(self.links)
             #self.links=list(set(self.links))    
 
-    def checkProduct(self):
-        with open('./prod.csv','rb') as input_file:
-            reader = csv.reader(input_file)
-            for row in reader:
-                for i in range(len(self.links)):
-                    if(row[1]==self.links[i]):
-                        self.main_column=self.columns[i]
-                        self.main_dictionary = self.size_dictionary[i]
 
-                id_prod=row[0].split('/')[1]
-                for single_column in self.main_column:
-                    if id_prod==single_column.find(class_="color-name").get_text().replace("\t"," ").split(' ')[0]:
-                        num=int(self.main_dictionary[row[0].split('/')[2]])
-                        available=int((single_column.find(class_="cBsecondLine").find_all(class_="assortTypeFixed"))[num].get_text().replace("+","").strip())
-                        if(available>0):
-                            print(row[0]+" product is available "+str(available))
-                        elif(available==0):
-                            print(row[0]+" product is not available")
                             
 
 inv = CheckInInventory();
-inv.checkProduct();
 
 
 
