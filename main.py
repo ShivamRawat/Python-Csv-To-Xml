1e6f4c6f9e4901817cb2ea6c4d9bc28b211017ec
Shivam Rawat <shivamrawat6670@gmail.com>
2019-04-22 15:21:41 +0530

    Functionality to send params

--- main.py
+++ main.py
@@ -49,19 +49,48 @@ class CheckInInventory:
                     index_value  =-1
                     for sizes in single_column.find(class_="tableBackgroundBlack").find_all("td"):
                         index_value+=1
-                        if(self.checkSizes(sizes.get_text(),row[0].split('/')[2])):
-                            if id_prod==single_column.find(class_="color-name").get_text().replace("\t"," ").split(' ')[0]:
+                        if id_prod==single_column.find(class_="color-name").get_text().replace("\t"," ").split(' ')[0]:
+                            if(self.checkSizes(sizes.get_text(),row[0].split('/')[2])):
                                 available=int((single_column.find(class_="cBsecondLine").find_all("td"))[index_value].get_text().replace("+","").strip())
                                 if(available>0):
                                     print(row[0]+" product is available "+str(available))
                                 elif(available==0):
                                     print(row[0]+" product is not available")
-                            
 
-inv = CheckInInventory();
-inv.checkProduct();
+class ReadFromHtml:
+    def __init__(self,sku,hmtllink):
+        self.sku=sku
+        self.pages=(requests.get(hmtllink))
+        self.soups=(BeautifulSoup(self.pages.content,'html.parser')) 
+        self.columns=(self.soups.find_all(class_="cBeditable"))
+        self.checkProduct()
+    def checkSizes(self,webSize,size):
+        if(size==webSize):
+            return True
+        elif(webSize=="XXXL" and size=="3XL"):
+            return True
+        elif(webSize=="XXL" and size=="2XL"):
+            return True
+        elif(webSize=="XXS" and size=="2XS"):
+            return True                 
+        else :  
+            return False               
+    def checkProduct(self):
+        for single_column in self.columns:
+            index_value  =-1
+            for sizes in single_column.find(class_="tableBackgroundBlack").find_all("td"):
+                index_value+=1
+                if self.sku.split('/')[1]==single_column.find(class_="color-name").get_text().replace("\t"," ").split(' ')[0]:
+                    if(self.checkSizes(sizes.get_text(),self.sku.split('/')[2])):
+                        available=int((single_column.find(class_="cBsecondLine").find_all("td"))[index_value].get_text().replace("+","").strip())
+                        if(available>0):
+                            print(self.sku+" product is available "+str(available))
+                        elif(available==0):
+                            print(self.sku+" product is not available")
+#inv = CheckInInventory();
+#inv.checkProduct();
 
-
+inv = ReadFromHtml("302510/0099DKTEAM/2XS","https://seizoroi.com/product_api/0999/0999_dk.htm")
 
 
 # Size not found 
